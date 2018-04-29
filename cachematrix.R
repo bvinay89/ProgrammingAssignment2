df63b1785ac3e48dfa14aa1c26d25990e828e04f
bvinay89 <vinay.banda89@gmail.com>
2018-04-30 00:30:50 +0530

    modified the cachematrix as per assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,59 @@
 
 ## Write a short comment describing this function
 
+## Initiating the x as matrix inside the function args
+
 makeCacheMatrix <- function(x = matrix()) {
 
+## Initialising the inverse of the matrix as inv to NULL
+
+   inv <- NULL
+
+   
+   set <- function(y) {
+
+## Assigning the y matrix to x (global variable initialised inside the fucntion args)     
+## Initialising the inverse of the matrix as inv to NULL
+## Similar to makeCacheMatrix function
+     x <<- y
+    inv <<- NULL
+   }
+## get function to get the matrix
+  get <- function() x
+## set the value of Inverse of the given matrix 
+  setsolve <- function(solve) inv <<- solve
+## get the value of Inverse of the given matrix  
+  getsolve <- function() inv
+  
+## deifining the names for the functions mentioned above  
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 
+## matrix is passed to the function as is assigned as x
+
 cacheSolve <- function(x, ...) {
+
+## Initialising inverse of the matrix by using getsolve function  
+  inv <- x$getsolve()
+  
+## condition to check if the inverse of the matrix inv is null or not. If inv is present it means the 
+## inverse is already calculated before and it gives message saying "getting cached data"
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+## if inverse of matrix is not present then the matrix is stored in data and then inverse is
+##calculated using solve function
+  
+  data <- x$get()
+  inv <- solve(data, ...)
+## set the inverse of the matrix value to inv by using setsolve function  
+  x$setsolve(inv)
+  inv
         ## Return a matrix that is the inverse of 'x'
 }
